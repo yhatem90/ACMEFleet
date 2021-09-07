5ff989ea0a64f5b847aaf85ee1ad4d0428fce37a
yhatem90 <90036168+yhatem90@users.noreply.github.com>
2021-09-07 22:07:51 +0300

    Delete ACMEFleet directory

--- ACMEFleet/src/main/java/com/vehicle/exceptions/VehicleNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.vehicle.exceptions;
-
-public class VehicleNotFoundException extends RuntimeException {
-
-	  VehicleNotFoundException(String vin) {
-	    super("Could not find vehicle  " + vin);
-	  }
-	}
